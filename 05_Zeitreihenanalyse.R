5dde43b9fea120e1666ba9ef9b5b9893784bee1f
vizualexander <mail@alexander-klein.ch>
2016-12-02 20:33:46 +0100

    Add files via upload

---
+++ 05_Zeitreihenanalyse.R
@@ -0,0 +1,28 @@
+#################################################################################################
+# Zeitreihenanalyse und Prediction + MAPE
+# 
+# URL: https://www.otexts.org/fpp/2/5
+#
+#################################################################################################
+
+library("fpp")
+beer2 <- window(ausbeer,start=1992,end=2006-.1)
+
+summary(ausbeer)
+
+beerfit1 <- meanf(beer2,h=11)
+beerfit2 <- rwf(beer2,h=11)
+beerfit3 <- snaive(beer2,h=11)
+
+
+plot(beerfit1, plot.conf=FALSE, main="Forecasts for quarterly beer production")
+lines(beerfit2$mean,col=2)
+lines(beerfit3$mean,col=3)
+lines(ausbeer)
+legend("topright", lty=1, col=c(4,2,3), legend=c("Mean method (meanf)","Naive method (rwf)","Seasonal naive method (snaive)"))
+
+
+beer3 <- window(ausbeer, start=2006)
+accuracy(beerfit1, beer3)
+accuracy(beerfit2, beer3)
+accuracy(beerfit3, beer3)
